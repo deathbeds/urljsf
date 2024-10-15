132aa35e4742ba1217881e739d86e5837ee0d8a0
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-15 08:40:11 -0500

    bare minimum cli building

--- src/urljsf/urljsf.py
+++ src/urljsf/urljsf.py
@@ -6,17 +6,18 @@ from __future__ import annotations
 
 import json
 import re
+from hashlib import sha256
 from logging import Logger, getLogger
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 import jinja2
 
-from .constants import STATIC, TEMPLATES, __dist__
+from .constants import MIME_PREFIX, STATIC, TEMPLATES, __dist__
 from .source import DefSource
 
 if TYPE_CHECKING:
-    from ._schema.form_schema import Urljsf as UrljsfSchema
+    from ._schema import Urljsf as UrljsfSchema
     from .config import Config
 
 
@@ -58,12 +59,11 @@ class Urljsf:
                 "Found validation errors: %s", cfg.definition.validation_errors
             )
 
-        self.deploy_form_files(cfg.output_dir)
-        # rendered = self.render()
-        # cfg.output_dir.mkdir(parents=True, exist_ok=True)
-        # out_html = cfg.output_dir / cfg.html_filename
-        # out_html.write_text(rendered, encoding="utf-8")
-        # self.deploy_static(cfg.output_dir / "_static")
+        rendered = self.render()
+        cfg.output_dir.mkdir(parents=True, exist_ok=True)
+        out_html = cfg.output_dir / cfg.html_filename
+        out_html.write_text(rendered, encoding="utf-8")
+        self.deploy_static(cfg.output_dir / "_static")
         return 1
 
     def load_definition(self) -> bool:
@@ -80,12 +80,6 @@ class Urljsf:
         if TYPE_CHECKING:
             assert self.config.definition.data is not None
         return self.config.definition.data
-
-    def deploy_form_files(self, out_path: Path) -> None:
-        """Copy the schema, uiSchema, and data files."""
-        defn = self.definition
-
-        raise NotImplementedError(defn)
 
     def from_file_or_py(
         self,
@@ -115,10 +109,11 @@ class Urljsf:
         return resolved
 
     @staticmethod
-    def write(in_bytes: bytes, name: str, static_path: Path) -> str:
+    def write_hashed(in_bytes: bytes, name: str, static_path: Path) -> str:
         """Write a file to disk with a hash-based cache buster."""
+        shasum = sha256(in_bytes).hexdigest()[:8]
         stem, ext = name.rsplit(".", 1)
-        out_path = static_path / f"{stem}.{ext}"
+        out_path = static_path / f"{stem}-{shasum}.{ext}"
         if not out_path.exists():
             out_path.parent.mkdir(parents=True, exist_ok=True)
             out_path.write_bytes(in_bytes)
@@ -130,12 +125,13 @@ class Urljsf:
         self.log.debug("rendering: %s", cfg)
         tmpl = self.env.get_template(cfg.template)
         context = dict(cfg.__dict__)
+        context.update(definition_json=self.definition, mime_prefix=MIME_PREFIX)
         return tmpl.render(context)
 
     @staticmethod
     def deploy_static(path: Path) -> None:
         """Copy the static assets into the right place."""
-        for child in STATIC.rglob("*"):
+        for child in (STATIC / "urljsf").rglob("*"):
             if child.is_dir():
                 continue
             rel = str(child.relative_to(STATIC))
