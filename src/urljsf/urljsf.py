4d69da2450220a7e16fd9cf5797bf5d1468d1293
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-16 18:56:17 -0500

    cut some more from core/cli

--- src/urljsf/urljsf.py
+++ src/urljsf/urljsf.py
@@ -4,12 +4,9 @@
 
 from __future__ import annotations
 
-import json
-import re
-from hashlib import sha256
 from logging import Logger, getLogger
 from pathlib import Path
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 import jinja2
 
@@ -52,10 +49,10 @@ class Urljsf:
         self.log.debug("config: %s", cfg)
         self.load_definition()
 
-        if not cfg.definition:
+        if not cfg.definition:  # pragma: no cover
             self.log.error("No definition found %s", self.config)
             return 1
-        if cfg.definition.validation_errors:
+        if cfg.definition.validation_errors:  # pragma: no cover
             self.log.error(
                 "Found validation errors: %s", cfg.definition.validation_errors
             )
@@ -71,7 +68,7 @@ class Urljsf:
         """Load a configuration from a file or dotted python module."""
         cfg = self.config
 
-        if cfg.input_ is None:
+        if cfg.input_ is None:  # pragma: no cover
             msg = f"No valid input from {cfg}"
             raise InvalidInputError(msg)
 
@@ -79,55 +76,18 @@ class Urljsf:
 
         if input_path.exists():
             cfg.definition = DefSource(input_path, log=self.log)
-        else:
+        else:  # pragma: no cover
             msg = "No urljsf definition found"
             raise InvalidDefinitionError(msg)
 
     @property
     def definition(self) -> UrljsfSchema:
         """Get the validated source."""
-        if self.config.definition is None or self.config.definition.data is None:
+        bad = self.config.definition is None or self.config.definition.data is None
+        if bad:  # pragma: no cover
             msg = "No urljsf definition found"
             raise InvalidDefinitionError(msg)
         return self.config.definition.data
-
-    def from_file_or_py(
-        self,
-        static_path: Path,
-        file_name: str | None,
-        dotted: str | None,
-    ) -> str | None:
-        """Resolve a source file or dotted python import to a static file."""
-        if dotted:
-            name, raw = self.import_dotted(dotted)
-            if isinstance(raw, str):
-                resolved = raw
-            elif isinstance(raw, dict):
-                in_bytes = json.dumps(raw, indent=2).encode("utf-8")
-                resolved = self.write_hashed(in_bytes, name, static_path)
-            else:  # pragma: no cover
-                msg = f"cannot resolve import as URL string or schema object: {raw}"
-                raise RuntimeError(msg)
-        elif file_name is None:
-            resolved = None
-        elif self.is_url(file_name):
-            resolved = file_name
-        else:
-            in_path = Path(file_name)
-            in_bytes = in_path.read_bytes()
-            resolved = self.write_hashed(in_bytes, in_path.name, static_path)
-        return resolved
-
-    @staticmethod
-    def write_hashed(in_bytes: bytes, name: str, static_path: Path) -> str:
-        """Write a file to disk with a hash-based cache buster."""
-        shasum = sha256(in_bytes).hexdigest()[:8]
-        stem, ext = name.rsplit(".", 1)
-        out_path = static_path / f"{stem}-{shasum}.{ext}"
-        if not out_path.exists():
-            out_path.parent.mkdir(parents=True, exist_ok=True)
-            out_path.write_bytes(in_bytes)
-        return out_path.name
 
     def render(self) -> str:
         """Render a template."""
@@ -148,26 +108,3 @@ class Urljsf:
             dest = path / rel
             dest.parent.mkdir(parents=True, exist_ok=True)
             dest.write_bytes(child.read_bytes())
-
-    def import_dotted(self, dotted: str) -> tuple[str, dict[str, Any] | str]:
-        """Generate a JSON file from a dotted python import."""
-        module_path, member = dotted.split(":")
-        submodules = module_path.split(".")[1:]
-        current = __import__(module_path)
-        for sub in submodules:
-            current = getattr(current, sub)
-        final = getattr(current, member)
-        if callable(final):
-            final = final()
-        return self.dotted_name(dotted), final
-
-    @staticmethod
-    def dotted_name(dotted: str) -> str:
-        """Get a filename for a dotted import."""
-        module_path, member = dotted.split(":")
-        return f"{module_path}-{member}.json".lower()
-
-    @staticmethod
-    def is_url(value: str | Path | None) -> bool:
-        """Get whether a value is a URL."""
-        return bool(re.findall("^https?://", str(value)))
